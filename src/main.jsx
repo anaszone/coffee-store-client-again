455eeb126f9c261f92b719b26723140e05b32651
anaszone <mohammedanaschowdhury@gmail.com>
2026-02-20 12:12:29 +0600

    layouts setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import MainLayout from "./layouts/MainLayout.jsx";
+import Home from "./components/Home.jsx";
+import AddCoffee from "./components/AddCoffee.jsx";
+import UpdateCoffee from "./components/UpdateCoffee.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        component: Home,
+      },
+      {
+        path: "addcoffee",
+        component: AddCoffee,
+      },
+      {
+        path: "updatecoffee",
+        component: UpdateCoffee,
+      },
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />,
   </StrictMode>,
-)
+);
